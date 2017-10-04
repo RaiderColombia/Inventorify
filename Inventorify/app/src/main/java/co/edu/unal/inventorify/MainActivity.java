08763d20e6c8bb1cbc5b78c3e853c33fefa31d09
Raidercolombia <raidercolombia@gmail.com>
2017-10-04 07:04:47 -0500

    Added more fields to the New Product Form Added more attributes to the Product entity Implemented the ZXing Codebar Scanner Library More product information is now inserted into the database

--- Inventorify/app/src/main/java/co/edu/unal/inventorify/MainActivity.java
+++ Inventorify/app/src/main/java/co/edu/unal/inventorify/MainActivity.java
@@ -1,7 +1,12 @@
 package co.edu.unal.inventorify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import co.edu.unal.inventorify.activities.product.NewProductActivity;
+import co.edu.unal.inventorify.barcodes.ZXingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newProduct(View view){
+        startActivity(new Intent(this, NewProductActivity.class));
     }
 }
--- Inventorify/app/src/main/java/co/edu/unal/inventorify/models/Product.java
+++ Inventorify/app/src/main/java/co/edu/unal/inventorify/models/Product.java
@@ -9,7 +9,10 @@ public class Product {
 
     public String uid;
     public String name;
+    public String barcode;
+    public int quantity;
     public int price;
+    public int cost;
 
     public Product(String uid){
         this.uid = uid;
@@ -23,4 +26,15 @@ public class Product {
         this.price = price;
     }
 
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
